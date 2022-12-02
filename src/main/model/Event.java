fc47d9c47e9e6c658746103e540b8d3097ea1327
Kevin Hu <kevinh64@student.ubc.ca>
2022-12-01 17:12:26 -0800

    fixing checkstyle

--- src/main/model/Event.java
+++ src/main/model/Event.java
@@ -43,11 +43,13 @@ public class Event {
 
     @Override
     public boolean equals(Object other) {
-        if (other == null)
+        if (other == null) {
             return false;
+        }
 
-        if (other.getClass() != this.getClass())
+        if (other.getClass() != this.getClass()) {
             return false;
+        }
 
         Event otherEvent = (Event) other;
 
